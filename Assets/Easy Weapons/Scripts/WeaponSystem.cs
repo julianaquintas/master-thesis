1748cd394dc6b43bb593c6e589dac4394f5a0de5
julianaquintas <50043313+julianaquintas@users.noreply.github.com>
2019-04-29 00:24:57 +0100

    Add files via upload

---
+++ Assets/Easy Weapons/Scripts/WeaponSystem.cs
@@ -0,0 +1,86 @@
+﻿/// <summary>
+/// WeaponSystem.cs
+/// Author: MutantGopher
+/// This script manages weapon switching.  It's recommended that you attach this to a parent GameObject of all your weapons, but this is not necessary.
+/// This script allows the player to switch weapons in two ways, by pressing the numbers corresponding to each weapon, or by scrolling with the mouse.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class WeaponSystem : MonoBehaviour
+{
+	public GameObject[] weapons;				// The array that holds all the weapons that the player has
+	public int startingWeaponIndex = 0;			// The weapon index that the player will start with
+	private int weaponIndex;					// The current index of the active weapon
+
+
+	// Use this for initialization
+	void Start()
+	{
+		// Make sure the starting active weapon is the one selected by the user in startingWeaponIndex
+		weaponIndex = startingWeaponIndex;
+		SetActiveWeapon(weaponIndex);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		// Allow the user to instantly switch to any weapon
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			SetActiveWeapon(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SetActiveWeapon(2);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SetActiveWeapon(5);
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+
+            SetActiveWeapon(6);
+		//if (Input.GetButtonDown("Weapon 5"))
+		//	SetActiveWeapon(4);
+		//if (Input.GetButtonDown("Weapon 6"))
+			//SetActiveWeapon(8);
+		/*if (Input.GetButtonDown("Weapon 7"))
+			SetActiveWeapon(6);
+		if (Input.GetButtonDown("Weapon 8"))
+			SetActiveWeapon(7);
+		if (Input.GetButtonDown("Weapon 9"))
+			SetActiveWeapon(8); */
+
+	}
+
+	void OnGUI()
+	{
+
+
+	}
+
+	public void SetActiveWeapon(int index)
+	{
+		// Make sure this weapon exists before trying to switch to it
+		if (index >= weapons.Length || index < 0)
+		{
+			Debug.LogWarning("Tried to switch to a weapon that does not exist.  Make sure you have all the correct weapons in your weapons array.");
+			return;
+		}
+
+		// Send a messsage so that users can do other actions whenever this happens
+		SendMessageUpwards("OnEasyWeaponsSwitch", SendMessageOptions.DontRequireReceiver);
+
+		// Make sure the weaponIndex references the correct weapon
+		weaponIndex = index;
+
+		// Make sure beam game objects aren't left over after weapon switching
+		weapons[index].GetComponent<Weapon>().StopBeam();
+
+		// Start be deactivating all weapons
+		for (int i = 0; i < weapons.Length; i++)
+		{
+			weapons[i].SetActive(false);
+		}
+
+		// Activate the one weapon that we want
+		weapons[index].SetActive(true);
+	}
+
+}
